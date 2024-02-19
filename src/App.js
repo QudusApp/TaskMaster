6bafe1a9f326da769790c74add264a03c91a882d
@Github3636 <qudusnafiu4@gmail.com>
2024-02-19 01:55:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [task, setTask] = useState([]);
+    const [newTask, setNewTask] = useState('')
+
+    useEffect(() => {
+        setTask(['To read my book by 8PM', 'To do the dishes']);
+    }, []);
+
+
+    const handleAddTask = () => {
+        if(newTask) {
+            setTask([...task, newTask]);
+            setNewTask('');
+        }
+    }
+
+    const handleDeleTask = (index) => {
+        const copyTask = task.slice();
+        copyTask.splice(index, 1);
+        setTask(copyTask);
+    }
+
+    return(
+        <div className="App">
+            <h1>Amatip IT Todo-List App</h1>
+            <input 
+            type="text" 
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Enter a new task"
+            />
+            <button onClick={handleAddTask}>Add</button>
+            
+            <ul>
+                {task.map((task, index) => (
+                    <li key={index}>
+                        {task}
+                        <button onClick={handleDeleTask}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        
+    )
 }
 
 export default App;
